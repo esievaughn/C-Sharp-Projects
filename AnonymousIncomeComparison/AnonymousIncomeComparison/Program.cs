d839961ade5d88d91d24fb155b3378c1d0abab3c
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-04-09 13:45:23 -0600

    Add files via upload
    get hourly rate and hours worked per week from person 1 and person 2; calculate individual salary; compare if person 1's salary is greater than person 2's salary

---
+++ AnonymousIncomeComparison/AnonymousIncomeComparison/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousIncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("hourly rate:");
+            double p1HourlyRate = Convert.ToDouble(Console.ReadLine());         // converting to double in case of decimals 
+            Console.WriteLine("hours worked per week:");
+            double p1HoursWorked = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nPerson 2");
+            Console.WriteLine("hourly rate:");
+            double p2HourlyRate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("hours worked per week:");
+            double p2HoursWorked = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nAnnual Salary of Person 1:");
+            double p1Salary = (p1HourlyRate * p1HoursWorked) * 52;         // 52 is the number of weeks in a year
+            Console.WriteLine(p1Salary);
+
+            Console.WriteLine("Annual Salary of Person 2:");
+            double p2Salary = (p2HourlyRate * p2HoursWorked) * 52;         // 52 is the number of weeks in a year
+            Console.WriteLine(p2Salary);
+
+            Console.WriteLine("\nDoes Person 1 make more money than Person 2?");
+            bool compareSalary = p1Salary > p2Salary;
+            Console.WriteLine(compareSalary);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
