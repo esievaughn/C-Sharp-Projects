a23ec1cf5c82e6834926a6fffb2adbf84197b92d
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-04-13 11:46:48 -0600

    Add files via upload
    Assignment to create a class with three methods that are called from the main program. Simple speed, time, distance calculations without measurement conversions like seconds/hours or mph/kilometers.

---
+++ CallingMethods/CallingMethods/SpeedDistanceTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class SpeedDistanceTime
+    {
+        public double Speed(double distance, double time)
+        {
+            double speedResult = distance / time;
+            return speedResult;
+        }
+        public double Distance(double speed2, double time2)
+        {
+            double distanceResult = speed2 * time2;
+            return distanceResult;
+        }
+        public double Time(double distance3, double speed3)
+        {
+            double timeResult = distance3 / speed3;
+            return timeResult;
+        }
+    }   
+}
