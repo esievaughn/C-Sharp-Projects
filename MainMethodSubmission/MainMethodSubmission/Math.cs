15ad277ab83dc761ae74548a8a958ca78b854a2e
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-04-13 13:27:56 -0600

    Add files via upload

---
+++ MainMethodSubmission/MainMethodSubmission/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    public class Math
+    {
+        public int timesTwo(int num)
+        {
+            return num * 2;
+        }
+        public decimal timesTwo(decimal dec)
+        {
+            return dec * 2 *2;
+        }
+        public int timesTwo(string str)
+        {
+            int strConvert = Convert.ToInt32(str);
+            return (strConvert * 2) / 2;
+        }
+    }
+}
