83874e0a0731b064e0a5bfd80bd0d965dbfec5e7
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-04-17 09:21:02 -0600

    Add files via upload

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/OptionalMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace MethodSubmissionAssignment
+{
+    public class OptionalMethod
+    {
+        public int OpMthd(int userNum1, [Optional] int userNum2) //Optional using Runtime.InteropServices does not require default value to be specified
+        {
+            return userNum1 + userNum2;
+        }
+    }
+}
