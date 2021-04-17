321929030422ce1654d66a2d55157eaa42506e81
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-04-17 09:50:43 -0600

    Add files via upload

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidClass displayVoid = new VoidClass();
+
+            Console.WriteLine("Please write a whole number below: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            displayVoid.Sub(50, 200); //call method passing in two numbers
+            displayVoid.Sub(num1, num2: 100); //call method specifying by name
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/VoidClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class VoidClass
+    {
+        public void Sub(int num1, int num2) //void method does not return values
+        {
+            int result = num1 - 10; //math performed on first integer
+            Console.WriteLine(num2); //only second integer is written to the console
+        }
+    }
+}
