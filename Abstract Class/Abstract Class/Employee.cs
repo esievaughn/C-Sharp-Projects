ff2188c91a57bb78808a4967a70919656d0c580c
esievaughn <78444940+esievaughn@users.noreply.github.com>
2021-04-18 08:57:16 -0600

    Add files via upload

---
+++ Abstract Class/Abstract Class/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Employee : Person
+    {
+        public override void SayName() //override for abstract method
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
